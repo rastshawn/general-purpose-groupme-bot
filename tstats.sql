2c85f6b494e0a8f3f435f5b4451f55e97349a809
Shawn Rast <rastshawn@gmail.com>
2018-11-27 21:58:39 -0600

    fixed date bug where sometimes it'd return eight records (when a user asked for tstats after posting t for the day, but before the time they asked for t the first day in the 7 day period)

--- tstats.sql
+++ tstats.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE week_average()
 BEGIN
     SELECT User.Name, AVG(TCount.t) AS AverageT, COUNT(TCount.t) AS numRecords FROM TCount
         JOIN User ON User.GroupMeUserID = TCount.GroupMeUserID
-        WHERE TCount.time BETWEEN (DATE(NOW()) - INTERVAL 7 DAY) AND NOW()
+        WHERE TCount.time BETWEEN (DATE(NOW()) - INTERVAL 6 DAY) AND NOW() -- 6 days instead of 7 because users complained when it returned eight records
         GROUP BY User.Name
         ORDER BY AverageT DESC;
 END //
